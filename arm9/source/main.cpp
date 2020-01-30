d440a238b313638e979965bb7782d7ec69ffb81c
RocketRobz <bobesh8@gmail.com>
2020-01-30 00:39:16 -0700

    Bump version number

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -85,7 +85,7 @@ int main(int argc, char **argv) {
 	
 	bool yHeld = false;
 
-	snprintf(titleName, sizeof(titleName), "GodMode9i v%i.%i.%i", 1, 3, 5);
+	snprintf(titleName, sizeof(titleName), "GodMode9i v%i.%i.%i", 2, 0, 0);
 
 	// initialize video mode
 	videoSetMode(MODE_4_2D);
