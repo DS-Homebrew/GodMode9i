d5b96dcb0b4b9d95e91e8e271b0f36e57d3e60b6
Pk11 <epicpkmn11@outlook.com>
2021-07-31 21:45:19 -0500

    Add trimming existing NDS ROMs (#104)

--- arm9/source/fileOperations.h
+++ arm9/source/fileOperations.h
@@ -24,6 +24,7 @@ extern void printBytesAlign(int bytes);
 
 extern off_t getFileSize(const char *fileName);
 extern bool calculateSHA1(const char *fileName, u8 *sha1);
+extern int trimNds(const char *fileName);
 extern int fcopy(const char *sourcePath, const char *destinationPath);
 void changeFileAttribs(DirEntry* entry);
 
