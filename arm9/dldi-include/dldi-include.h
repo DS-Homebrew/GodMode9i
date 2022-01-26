b2c9e58291c01a11f2c8d2400e73360a0eec9faf
Pk11 <epicpkmn11@outlook.com>
2022-01-26 14:56:12 -0600

    Remove unused DLDIs (#157)

--- arm9/dldi-include/dldi-include.h
+++ arm9/dldi-include/dldi-include.h
@@ -1,23 +1,9 @@
 #ifndef DLDIINCLUDE_H
 #define DLDIINCLUDE_H
 
-extern unsigned char r4_sd_dldi[];
-//extern unsigned int r4_sd_dldi_len;
-extern unsigned char r4tf_dldi[];
-//extern unsigned int r4tf_dldi_len;
-extern unsigned char tt_sd_dldi[];
-//extern unsigned int tt_sd_dldi_len;
-extern unsigned char ttio_dldi[];
-//extern unsigned int ttio_dldi_len;
-extern unsigned char CycloEvo_dldi[];
-//extern unsigned int CycloEvo_dldi_len;
-extern unsigned char dsx_dldi[];
-//extern unsigned int dsx_dldi_len;
 extern unsigned char r4idsn_sd_dldi[];
 //extern unsigned int r4idsn_sd_dldi_len;
 extern unsigned char ak2_sd_dldi[];
 //extern unsigned int ak2_sd_dldi_len;
-extern unsigned char dstwo_dldi[];
-//extern unsigned int dstwo_dldi_len;
 
 #endif //DLDIINCLUDE_H
