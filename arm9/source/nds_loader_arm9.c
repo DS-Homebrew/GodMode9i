166258b5647364108f753616199e0cc92a2ff9f1
RocketRobz <bobesh8@gmail.com>
2024-11-11 13:51:10 -0700

    Update nds_loader_arm9.c

--- arm9/source/nds_loader_arm9.c
+++ arm9/source/nds_loader_arm9.c
@@ -274,7 +274,6 @@ int runNds (const void* loader, u32 loaderSize, u32 cluster, bool initDisc, bool
 
 	writeAddr ((data_t*) LCDC_BANK_C, DSIMODE_OFFSET, isDSiMode());
 	if(argv[0][0]=='s' && argv[0][1]=='d') {
-		dldiPatchNds = false;
 		writeAddr ((data_t*) LCDC_BANK_C, HAVE_DSISD_OFFSET, 1);
 	}
 
@@ -373,7 +372,7 @@ int runNdsFile (const char* filename, int argc, const char** argv)  {
 	installBootStub(havedsiSD);
 	#endif */
 
-	return runNds (load_bin, load_bin_size, st.st_ino, true, true, argc, argv);
+	return runNds (load_bin, load_bin_size, st.st_ino, true, (memcmp(io_dldi_data->friendlyName, "Default", 7) != 0), argc, argv);
 }
 
 /*
