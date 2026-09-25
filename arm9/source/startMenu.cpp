ae9255f568782227d70343f06a4ea4f52fd385f5
Marcos Raffael <81723572+raffael180@users.noreply.github.com>
2026-09-25 23:48:00 +0000

    Update list to 19 (Add pt-BR language)

--- arm9/source/startMenu.cpp
+++ arm9/source/startMenu.cpp
@@ -28,7 +28,7 @@ constexpr std::array<std::string *, 4> startMenuStrings = {
 	&STR_LANGUAGE
 };
 
-constexpr std::array<std::pair<const char *, const char *>, 18> languageList = {{
+constexpr std::array<std::pair<const char *, const char *>, 19> languageList = {{
 	{"id-ID", "Bahasa Indonesia"},
 	{"de-DE", "Deutsch"},
 	{"en-US", "English"},
